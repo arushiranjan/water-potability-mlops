19f2863544ec4aca5f8e98304f85b6685b665329
Arushi Ranjan <arushi.ranjan26@gmail.com>
2026-08-25 13:03:12 +0530

    Deploy using FASTAPI and add parameter tuning in model_building

--- src/model_building.py
+++ src/model_building.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import os
 import pickle
+import yaml
 
 from sklearn.ensemble import RandomForestClassifier
 
+# Load parameters
+n_estimators = yaml.safe_load(open("params.yaml", "r"))["model_building"]["n_estimators"]
+
 train_data = pd.read_csv("./data/processed/train_processed.csv")
 
-X_train = train_data.iloc[:, 0:-1].values
-y_train = train_data.iloc[:, -1].values
+# X_train = train_data.iloc[:, 0:-1].values
+# y_train = train_data.iloc[:, -1].values
+X_train = train_data.drop(columns=['Potability'])
+y_train = train_data['Potability']
 
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(n_estimators=n_estimators)
 
 clf.fit(X_train, y_train)
 
